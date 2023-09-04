a4dfc44ee9161006782367a04e7f28b53d77822f
Gabriel Omar Berdejo Pareja <gabrielberdejo96@gmail.com>
2023-09-03 23:49:53 -0500

    changed config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-import { UtilsService } from './utils/utils.service';
-import { UtilsModule } from './utils/utils.module';
-
-export { UtilsService, UtilsModule };
+export * from './utils/utils.service';
+export * from './utils/utils.module';
